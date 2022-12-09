70c337531e15e5a790027ec4d14a200ae30e1e2f
gougisle <lefebagougis@gmail.com>
2022-12-09 09:45:09 -0800

    run vercel ncc build

--- dist/index.js
+++ dist/index.js
@@ -9689,17 +9689,9 @@ const core = __nccwpck_require__(2186);
 const github = __nccwpck_require__(5438);
 
 async function run() {
-  const GITHUB_TOKEN = core.getInput("GITHUB_TOKEN");
-  const octokit = github.getOctokit(GITHUB_TOKEN);
+  const PULL_REQUEST = core.getInput("PULL_REQUEST");
 
-  const { context = {} } = github;
-  const { pull_request } = context.payload;
-
-  await octokit.issues.createComment({
-    ...context.repo,
-    issues_number: pull_request.number,
-    body: "Thank you for the pull request we will review asap.",
-  });
+  console.log(PULL_REQUEST);
 }
 
 run();
