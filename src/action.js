ff4f509edb33841d7f0638fc785d6372b73358af
gougisle <lefebagougis@gmail.com>
2022-12-09 09:55:24 -0800

    saving work

--- src/action.js
+++ src/action.js
@@ -3,9 +3,9 @@ const core = require("@actions/core");
 const github = require("@actions/github");
 
 async function run() {
-  const PULL_REQUEST = core.getInput("PULL_REQUEST");
+  //const PULL_REQUEST = core.getInput("PULL_REQUEST");
 
-  console.log(PULL_REQUEST);
+  console.log($github.event.pull_request);
 }
 
 run();
