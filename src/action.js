680527b933b2106a1dbaa1386680038ddec0df87
gougisle <lefebagougis@gmail.com>
2022-12-09 09:44:06 -0800

    testing new input

--- src/action.js
+++ src/action.js
@@ -3,17 +3,9 @@ const core = require("@actions/core");
 const github = require("@actions/github");
 
 async function run() {
-  const GITHUB_TOKEN = core.getInput("GITHUB_TOKEN");
-  const octokit = github.getOctokit(GITHUB_TOKEN);
+  const PULL_REQUEST = core.getInput("PULL_REQUEST");
 
-  const { context = {} } = github;
-  const { pull_request } = context.payload;
-
-  await octokit.issues.createComment({
-    ...context.repo,
-    issue_number: pull_request.number,
-    body: "Thank you for the pull request we will review asap.",
-  });
+  console.log(PULL_REQUEST);
 }
 
 run();
