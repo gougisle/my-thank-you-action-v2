fa0838f7a946d35eb0f1570cc568e14a3720279c
gougisle <lefebagougis@gmail.com>
2022-12-09 10:32:21 -0800

    simplifying the action

--- src/action.js
+++ src/action.js
@@ -1,10 +1,8 @@
 //This is JS scripte we will run. This is our Action script
-const core = require("@actions/core");
-const github = require("@actions/github");
+//const core = require("@actions/core");
+//const github = require("@actions/github");
 
 async function run() {
-  //const PULL_REQUEST = core.getInput("PULL_REQUEST");
-
   console.log("action.js is running...");
 }
 
